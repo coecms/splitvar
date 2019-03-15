9ae36b2844907052c69730b7a8e9fdcaf96e300f
Aidan Heerdegen <aidan.heerdegen@gmail.com>
2019-03-15 16:33:53 +1100

    New version with command line interface (#2)
    * Changed deprecated API call
    
    * Got tests working with python3
    
    * Added cli.py.
    
    Added extra command line arguments.
    
    Can specify extra metadata files (like grid)
    
    Automatically search for dependent variables.
    
    * Renamed to groupbytime and put splitbytime back so tests pass
    
    * Added option to exclude variables (-x). Might be quicker than including.
    
    Check groupbytime works, don't save output when it doesn't
    
    Added option to delete global attributes
    
    * Added default value for --add option
    
    * Added data_vars='minimal' argument to open_mfdataset otherwise dimension
    vars were getting a time dimension added.
    
    * Enclosed makedirs in try/except block
    
    * Removed python 2.7 from travis CI as it doesn't support return
    from a generator.
    
    * Added function to match coordinates by units to identify lat/lon
    
    * Added logic to look for dependent vars in dependent vars, so it
    picks up things like time_bounds.

--- splitvar/splitvar.py
+++ splitvar/splitvar.py
@@ -8,6 +8,8 @@ import argparse
 import xarray
 import pandas as pd
 import netCDF4 as nc
+import cftime
+from copy import deepcopy
 
 def nested_groupby(dataarray, groupby):
     """From https://github.com/pydata/xarray/issues/324#issuecomment-265462343"""
@@ -38,7 +40,7 @@ def splitbytime(var, freq, timedim='time'):
         # raise ValueError("Split frequency ({}) is higher than data frequency ({}): not supported".format(freq,strfdelta(vardelta,"{D}d {H}h {M}m {S}s")))
         raise ValueError("Split frequency ({}) is higher than data frequency ({}): not supported".format(freq,vardelta))
 
-    if (type(var.indexes[timedim][0]) == nc.netcdftime._netcdftime.DatetimeNoLeap):
+    if (type(var.indexes[timedim][0]) == cftime.DatetimeNoLeap):
         # Variable is using a non-standard noleap calendar
         # Create a temporary pandas data frame with the start and end extracted time dimension
         # and the same frequency, but this has leap years (not an issue for what we will use this)
@@ -50,49 +52,107 @@ def splitbytime(var, freq, timedim='time'):
 
     # Use pandas time grouping to cycle through at freq, grabbing out
     # the start and end points to select out data
-    for k, v in pdtime.groupby(pd.TimeGrouper(freq=freq)):
+    for k, v in pdtime.groupby(pd.Grouper(freq=freq)):
         s, e = v.index.values[[0,-1]]
         yield var.sel(time=slice(pd.Timestamp(s),pd.Timestamp(e)))
 
-def splitbyvar(ds,vars=None,skipvars=['time']):
+def groupbytime(var, freq, timedim='time'):
+    """
+    Given an xarray variable, split into periods of time defined by freq
+    """
+    try:
+        for k, v in var.groupby(freq):
+            yield v
+    except KeyError:
+        return None
 
+def splitbyvar(ds,vars=None,skipvars=['time']):
+    """
+    Given an xarray variable, split into separate variables
+    """
     if vars is None:
-        selectvars = set(ds.data_vars)
+        vars = set(ds.data_vars)
     else:
-        selectvars = set(vars)
+        vars = set(vars).intersection(set(ds.data_vars))
 
     if skipvars is not None:
         for varname in skipvars:
-            selectvars.discard(varname)
-            selectvars.discard(varname.upper)
-            selectvars.discard(varname.lower)
+            vars.discard(varname)
+            vars.discard(varname.upper)
+            vars.discard(varname.lower)
 
-    for var in ds.data_vars:
-        if var in vars and var not in skipvars:
-            yield ds[var]
+
+    for var in vars:
+        yield var
+
+def getdependentvars(ds, var, skip_attrs=['long_name', 'standard_name', 'name', 'description']):
+    """
+    Find other variables upon which var depends
+    """
+    depvars = set()
+    # Cycle through all the variables to be selected
+    # and check if they depend on any other variables
+    for attr in ds[var].attrs:
+        if attr in skip_attrs:
+            continue
+        for attvar in list(ds.data_vars) + list(ds.coords):
+            if attvar == var: 
+                continue
+            # print(var,attvar,attr,ds[var].attrs[attr])
+            if attvar in ds[var].attrs[attr]:
+                # Variable is mentioned in an attribute
+                # so should also be copied 
+                depvars.add(attvar)
+
+    depdepvars = set()
+    for var in depvars:
+        # One level of recursion to make sure we get all the 
+        # dependencies of the variables/coordindates we just
+        # found 
+        depdepvars = depdepvars.union(getdependentvars(ds, var, skip_attrs))
+
+    return list(depvars.union(depdepvars))
 
 def genfilepath(var):
-    """Generate a file "path" from the name of the variable and it's frequency
+    """
+    Generate a file "path" from the name of the variable 
+    and it's frequency
     """
 
 def writevar(var,filename):
-    """Save variable to netcdf file
     """
+    Save variable to netcdf file
+    """
+    print('Saving data to {fname}'.format(fname=filename))
     var.to_netcdf(path=filename,format="NETCDF4_CLASSIC")
 
 def open_files(file_paths, freq):
 
-    # try and catch decode_times issues
-    ds = xarray.open_mfdataset(file_paths, engine='netcdf4', decode_times=False, mask_and_scale=True)
+    ds = xarray.open_mfdataset(file_paths, 
+                               decode_cf=False, 
+                               engine='netcdf4', 
+                               data_vars='minimal',
+                               mask_and_scale=True)
 
-if __name__ == "__main__":
+    for v in ds:
+        if 'chunksizes' in ds[v].encoding:
+            ds[v].chunk(ds[v].encoding['chunksizes'])
 
-    parser = argparse.ArgumentParser(description="Split multiple netCDF files by time and variable")
+    return ds
 
-    parser.add_argument("-v","--verbose", help="Verbose output", action='store_true')
+def findmatchingvars(ds, attname='units', matchstrings=[], ignorecase=True):
 
-    parser.add_argument("inputs", help="netCDF files or directories", nargs='+')
-    args = parser.parse_args()
+    matchvars = []
 
-    open_files(args, freq='year')
-    
+    if ignorecase:
+        transcase = lambda x: x
+    else:
+        transcase = lambda x: x.lower()
+
+    for var in ds.coords:
+        if 'units' in ds[var].attrs:
+            for matchstring in matchstrings:
+                if transcase(matchstring) in transcase(ds[var].attrs[attname]):
+                    matchvars.append(var)
+
+    return matchvars
